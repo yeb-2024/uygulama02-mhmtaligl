1778866145fcaa22e60aae8df8a45ab1991413d8
mehmet ali <mgul21969@gmail.com>
2025-02-14 18:06:25 +0300

    Kullanıcı Adı Girşi oluşturuldu

--- Uygulama02/src/GirişSayfası.java
+++ Uygulama02/src/GirişSayfası.java
@@ -4,12 +4,31 @@ import java.util.jar.JarFile;
 
 public class GirişSayfası {
     JFrame frame;
+    JTextField kullaniciAdi;
+
 
     public GirişSayfası(){
+
+        kullaniciAdi = new JTextField();
+        kullaniciAdi.setPreferredSize(new Dimension(200,40));
+
+        JPanel ustPanel = new JPanel();
+        ustPanel.setLayout(new FlowLayout());
+        JLabel ustLabel = new JLabel("Kullanıcı Adı:");
+        ustPanel.setFont(new Font("Arial",Font.PLAIN,20));
+
+        ustPanel.add(ustLabel);
+        ustPanel.add(kullaniciAdi);
+
+
+
         frame = new JFrame("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,300);
         frame.setLayout(new GridLayout(3,1));
         frame.setVisible(true);
+
+        frame.add(ustPanel);
+
     }
 }
